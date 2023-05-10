80b6ac42c81457e962a4666ea0e450dacc2b954c
ackerman <julio.sergio2709@gmail.com>
2023-05-10 14:09:33 -0400

    Add: edge runtime for "/" page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image'
 
+export const config = {
+  runtime: 'edge-runtime',
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
