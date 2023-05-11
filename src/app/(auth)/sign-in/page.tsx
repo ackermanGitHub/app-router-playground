f21dd39ad071914c39875203346d6cd36620f021
ackerman <julio.sergio2709@gmail.com>
2023-05-10 21:10:12 -0400

    setting up clerk middleware

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,20 @@
+import { SignIn } from "@clerk/nextjs/app-beta";
+
+interface PageProps {
+    searchParams: {
+        [key: string]: unknown;
+    };
+}
+export default function Page({ searchParams }: PageProps) {
+    const { redirect_url: redirectUrlFromParams } = searchParams || {};
+
+    let redirectUrl = "/";
+    if (redirectUrlFromParams && typeof redirectUrlFromParams === "string") {
+        redirectUrl = redirectUrlFromParams;
+    }
+
+    return <SignIn signUpUrl="/sign-up" redirectUrl={redirectUrl} />;
+}
+
+export const runtime = "experimental-edge";
+export const revalidate = 0;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,20 @@
+import { SignUp } from "@clerk/nextjs/app-beta";
+
+interface PageProps {
+    searchParams: {
+        [key: string]: unknown;
+    };
+}
+export default function Page({ searchParams }: PageProps) {
+    const { redirect_url: redirectUrlFromParams } = searchParams || {};
+
+    let redirectUrl = "/";
+    if (redirectUrlFromParams && typeof redirectUrlFromParams === "string") {
+        redirectUrl = redirectUrlFromParams;
+    }
+
+    return <SignUp signInUrl="/sign-in" redirectUrl={redirectUrl} />;
+}
+
+export const runtime = "experimental-edge";
+export const revalidate = 0;
