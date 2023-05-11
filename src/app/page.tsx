c2821bf8a652f5928e56639c6a157dbae95216e4
ackerman <julio.sergio2709@gmail.com>
2023-05-11 14:23:38 -0400

    fix uploader git bug 2.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { MultiUploader } from "@/components/Uploader"
 
 export const runtime = "edge"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <MultiUploader />
     </main>
   )
 }
