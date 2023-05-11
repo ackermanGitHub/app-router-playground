2ef817c76d9d3b2813e576fd606a88b2f84ad22e
ackerman <julio.sergio2709@gmail.com>
2023-05-11 12:53:29 -0400

    middleware and admin

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,15 @@
+
+export const metadata = {
+    title: 'Admin',
+    description: 'Nextjs app router playground',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className='w-1/2 h-1/2 border-solid border-2 border-red-500'></div>
+    )
+}
