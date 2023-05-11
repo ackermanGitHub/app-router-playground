65f934ca10560d1e05c0f1a2adef9e42e3c274fb
ackerman <julio.sergio2709@gmail.com>
2023-05-11 15:05:09 -0400

    asdasd

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { NextResponse, type NextRequest } from 'next/server';
 import { getAuth, withClerkMiddleware } from '@clerk/nextjs/server';
 
 const publicPaths = ['/sign-in*', '/sign-up*', '/api*'];
-const protectedPaths = ['/admin/*'];
 
 const isPublic = (reqPath: string) => {
   return publicPaths.find((publicPath) =>
@@ -10,25 +9,7 @@ const isPublic = (reqPath: string) => {
   );
 };
 
-const isProtected = (reqPath: string) => {
-  return protectedPaths.find((protectedPath) =>
-    reqPath.match(new RegExp(`^${protectedPath}$`.replace('*$', '($|/)')))
-  );
-};
-
 export default withClerkMiddleware((request: NextRequest) => {
-  if (isPublic(request.nextUrl.pathname)) {
-    return NextResponse.next();
-  }
-
-  if (isProtected(request.nextUrl.pathname)) {
-    const { userId } = getAuth(request);
-    if (!userId) {
-      const signInUrl = new URL('/sign-in', request.url);
-      signInUrl.searchParams.set('redirect_url', request.url);
-      return NextResponse.redirect(signInUrl);
-    }
-  }
   return NextResponse.next();
 });
 
