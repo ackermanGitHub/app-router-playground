a4a194b2fa6a6fd88bf695fdc4314b4eb1ddeb31
ackerman <julio.sergio2709@gmail.com>
2023-05-11 14:26:31 -0400

    Add: uploader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { MultiUploader } from "@/components/Uploader"
 
 export const runtime = "edge"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <MultiUploader />
     </main>
   )
 }
