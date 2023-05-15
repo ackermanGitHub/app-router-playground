3bda61caea1c8a8f8f6b64ff9161300e020f4b3b
ackerman <julio.sergio2709@gmail.com>
2023-05-15 13:58:19 -0400

    🖌️: styling 3.0

--- src/app/todos/columns.tsx
+++ src/app/todos/columns.tsx
@@ -15,6 +15,14 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
 
 export const columns: ColumnDef<z.infer<typeof todoSchema>>[] = [
     {
@@ -71,18 +79,6 @@ function TagCell({ value, todo_id }: { value: string, todo_id: number }) {
                     }
                 </SelectGroup>
             </SelectContent>
-            {/* <select
-            onChange={(event) => {
-                event.preventDefault();
-                event.stopPropagation();
-                logDev("data-table callback", event.target.value)
-            }}
-            defaultValue="To Do" className="bg-transparent text-primary decoration-transparent"
-        >
-            <option value="To Do">To Do</option>
-            <option value="Doing">Doing</option>
-            <option value="Done">Done</option>
-        </select> */}
         </Select>
     )
 }
@@ -91,41 +87,53 @@ function TitleCell({ value, todo_id }: { value: string, todo_id: number }) {
     const { editInput } = useInput()
     const [, startTransition] = useTransition()
     return (
-        <div onClick={(event) => {
-            event.preventDefault();
-            event.stopPropagation();
-            const target =
-                event.currentTarget.parentElement;
-            if (!target) {
-                throw new Error('target not found at ProductsTable');
-            }
-            const position = {
-                x: target.offsetLeft,
-                y: target.offsetTop,
-            };
-            const size = {
-                width: target.offsetWidth,
-                height: target.offsetHeight,
-            };
-            editInput.setLeft(position.x);
-            editInput.setTop(position.y);
-            editInput.setHeight(size.height);
-            editInput.setWidth(size.width);
-            editInput.setValue(value);
-            editInput.setActive(true);
-            editInput.setCallback(() => {
-                return (title: string) => {
-                    if (title !== value) {
-                        startTransition(() => updateTodo({ title, todo_id: todo_id }))
-                        logDev("✅✅✅ title update done", title)
-                    } else {
-                        logDev("❌❌❌ title update not done")
-                    }
+        <div className="flex justify-between gap-2 text-primary w-full h-full min-w-[80px] min-h-[20px]">
+            <Sheet >
+                <SheetTrigger>{value}</SheetTrigger>
+                <SheetContent>
+                    <SheetHeader>
+                        <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+                        <SheetDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
+            <svg onClick={(event) => {
+                event.preventDefault();
+                event.stopPropagation();
+                const target =
+                    event.currentTarget.parentElement?.parentElement;
+                if (!target) {
+                    throw new Error('target not found at ProductsTable');
                 }
-            })
+                const position = {
+                    x: target.offsetLeft,
+                    y: target.offsetTop,
+                };
+                const size = {
+                    width: target.offsetWidth,
+                    height: target.offsetHeight,
+                };
+                editInput.setLeft(position.x);
+                editInput.setTop(position.y);
+                editInput.setHeight(size.height);
+                editInput.setWidth(size.width);
+                editInput.setValue(value);
+                editInput.setActive(true);
+                editInput.setCallback(() => {
+                    return (title: string) => {
+                        if (title !== value) {
+                            startTransition(() => updateTodo({ title, todo_id: todo_id }))
+                            logDev("✅✅✅ title update done", title)
+                        } else {
+                            logDev("❌❌❌ title update not done")
+                        }
+                    }
+                })
 
-        }} className="text-primary w-fit min-w-[80px] min-h-[20px]">
-            {value}
+            }} viewBox="0 0 16 16" width={16} height={16} display="block" fill="currentColor" ><path d="M3.926 13.307H14.11c.183 0 .34-.066.472-.199a.644.644 0 00.198-.471.652.652 0 00-.198-.479.644.644 0 00-.472-.198H5.272l-1.346 1.347zm-.704-.636l7.683-7.684-1.312-1.319-7.69 7.684-.67 1.606c-.037.1-.017.191.06.273.083.082.174.105.274.069l1.655-.63zm8.34-8.326l.738-.732c.182-.187.278-.376.287-.567.009-.192-.068-.374-.232-.547l-.267-.267c-.169-.168-.351-.246-.547-.232-.196.014-.385.11-.567.287l-.739.732 1.327 1.326z"></path></svg>
         </div>
     )
 }
