76ee472abc0949d9d3dd18eb5bfdf3fb4dffb186
ackerman <julio.sergio2709@gmail.com>
2023-05-16 00:01:19 -0400

    ➕: Update tag

--- src/app/todos/columns.tsx
+++ src/app/todos/columns.tsx
@@ -37,7 +37,7 @@ export const columns: ColumnDef<z.infer<typeof todoSchema>>[] = [
     {
         accessorKey: "title",
         header: "Title",
-        cell({ getValue, row }) {
+        cell({ row }) {
             return (
                 <TitleCell todo={row.original} />
             )
@@ -66,7 +66,12 @@ function TagCell({ todo }: { todo: z.infer<typeof todoSchema> }) {
     const [, startTransition] = useTransition()
     const tag = todo.tags ? todo.tags[0] : "Select"
     return (
-        <Select defaultValue={tag}>
+        <Select
+            onValueChange={(value) => {
+                startTransition(() => updateTodo({ tags: [value], todo_id: todo.todo_id }))
+            }}
+            defaultValue={tag}
+        >
             <SelectTrigger className="w-[90px]">
                 <SelectValue placeholder={tag} />
             </SelectTrigger>
@@ -134,7 +139,7 @@ function TitleCell({ todo }: { todo: z.infer<typeof todoSchema> }) {
                     }
                 })
 
-            }} viewBox="0 0 16 16" className="min-h-[16px] min-w-[16px]" width={16} height={16} display="block" fill="currentColor" ><path d="M3.926 13.307H14.11c.183 0 .34-.066.472-.199a.644.644 0 00.198-.471.652.652 0 00-.198-.479.644.644 0 00-.472-.198H5.272l-1.346 1.347zm-.704-.636l7.683-7.684-1.312-1.319-7.69 7.684-.67 1.606c-.037.1-.017.191.06.273.083.082.174.105.274.069l1.655-.63zm8.34-8.326l.738-.732c.182-.187.278-.376.287-.567.009-.192-.068-.374-.232-.547l-.267-.267c-.169-.168-.351-.246-.547-.232-.196.014-.385.11-.567.287l-.739.732 1.327 1.326z"></path></svg>
+            }} viewBox="0 0 16 16" className="cursor-pointer min-h-[16px] min-w-[16px]" width={16} height={16} display="block" fill="currentColor" ><path d="M3.926 13.307H14.11c.183 0 .34-.066.472-.199a.644.644 0 00.198-.471.652.652 0 00-.198-.479.644.644 0 00-.472-.198H5.272l-1.346 1.347zm-.704-.636l7.683-7.684-1.312-1.319-7.69 7.684-.67 1.606c-.037.1-.017.191.06.273.083.082.174.105.274.069l1.655-.63zm8.34-8.326l.738-.732c.182-.187.278-.376.287-.567.009-.192-.068-.374-.232-.547l-.267-.267c-.169-.168-.351-.246-.547-.232-.196.014-.385.11-.567.287l-.739.732 1.327 1.326z"></path></svg>
         </div>
     )
 }
