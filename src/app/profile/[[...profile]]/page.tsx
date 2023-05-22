4d22f2de32b8ab6f1943b3a6382faabc20eb6704
ackerman <julio.sergio2709@gmail.com>
2023-05-22 12:32:53 -0400

    🔄️: profile Dynamic Route & min-[768px]:h-screen

--- src/app/profile/[[...profile]]/page.tsx
+++ src/app/profile/[[...profile]]/page.tsx
@@ -4,11 +4,10 @@ import {
 import { dark } from '@clerk/themes';
 
 export default async function ProfilePage() {
-
     return (
-        <UserProfile appearance={{
+        <UserProfile path="/profile" appearance={{
             elements: {
-                rootBox: "w-full h-full",
+                rootBox: "w-full h-full min-[768px]:h-screen",
                 card: "m-0 w-full max-w-none h-full rounded-none"
             },
             baseTheme: dark
