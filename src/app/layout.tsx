30c58ac169c823b495cd10fea4d610e14374a548
ackerman <julio.sergio2709@gmail.com>
2023-05-27 03:24:38 -0400

    🧙‍♂️🖌️: gradient slate-indigo bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <html style={{
         colorScheme: "dark",
       }} className='dark' lang="en">
-        <body className={`${inter.className}`}>
+        <body className={`${inter.className} bg-gradient-to-r from-slate-900 to-indigo-950`}>
           <Navbar />
           <main className='max-[600px]:mb-20 min-[600px]:ml-20'>
             {children}
