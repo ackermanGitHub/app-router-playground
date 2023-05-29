fe4a85df7abf78a01b24fbb2f81a978f4f3a96c3
ackerman <julio.sergio2709@gmail.com>
2023-05-28 23:08:17 -0400

    ➕: remove     .fileTypes(['image', 'video'])     .maxSize('1GB')

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -7,8 +7,6 @@ export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f
     // Set permissions and file types for this FileRoute
-    .fileTypes(['image', 'video'])
-    .maxSize('1GB')
     .onUploadComplete(async ({ file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log('Upload complete:', file);
