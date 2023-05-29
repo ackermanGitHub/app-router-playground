aa915d8ec1239a823141cebcdf9dd5a599f2b1de
ackerman <julio.sergio2709@gmail.com>
2023-05-29 17:04:09 -0400

    ➕: nextjs svg icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import VercelPGIco from "@/utils/SVGs"
+import { VercelPGIco, NextjsIco, NextjsRoundedIco } from "@/utils/SVGs"
 
 export default function HomePage() {
 
   return (
     <div className="flex h-auto flex-col items-center justify-center">
       <VercelPGIco />
+      <NextjsIco />
+      <NextjsRoundedIco />
     </div>
   )
 }
