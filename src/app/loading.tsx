b195c091a95d88a9885a34b8b64a03a04df13497
ackerman <julio.sergio2709@gmail.com>
2023-05-31 05:00:39 -0400

    🖌️

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,6 @@
 
-interface loadingProps {
 
-}
-
-const Loading: React.FC<loadingProps> = ({ }) => {
+const Loading = () => {
     return (
         <div className="flex flex-col items-center justify-center h-[80vh] text-gray-700 dark:text-slate-400">
             <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-zinc-700 dark:border-zinc-400" />
