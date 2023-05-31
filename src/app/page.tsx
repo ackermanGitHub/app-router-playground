421fc8fadcd348e26919c5cb429d794dcfe34bfe
ackerman <julio.sergio2709@gmail.com>
2023-05-31 01:13:44 -0400

    ➕🔄️gap to horizontal centered sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,52 +16,52 @@ export default function HomePage() {
   return (
     <div className="flex h-auto flex-col items-center justify-center">
       <Divider />
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
 
         <PostgresIcoSVG />
         <VercelPGIcoSVG />
       </div>
       <Divider />
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
 
         <EsLintIcoSVGs />
         <EsLintHeaderSVG />
         <EsLintIcoSVG />
       </div>
       <Divider />
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
 
         <RadixIcoSVGs />
         <RadixHeaderSVG />
         <RadixIcoSVG />
       </div>
       <Divider />
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
 
         <AutoprefixerSVG />
         <PostCssIco />
       </div>
       <TailwindIcoSVG />
       <Divider />
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
 
         <NextjsIcoSVG />
         <NextjsRoundedIcoSVG />
       </div>
       <Divider />
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
 
         <TypeScriptIcoSVG />
         <PrettierIco />
       </div>
       <Divider />
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
 
         <TanStackHeader className='h-[58px] flex items-center' />
         <ZodIcoSVG />
       </div>
       <Divider />
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
 
         <ClerkIcoSVG />
         <UploadThingHeader className='text-5xl h-[58px] flex items-center' />
