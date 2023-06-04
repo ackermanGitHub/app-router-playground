a48271ce1079c65ebcada7de72257d4c955042d5
ackerman <julio.sergio2709@gmail.com>
2023-06-04 03:01:09 -0400

    🖌️➕: Links elements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='bg-gray-100 dark:bg-gray-900 text-center'>
+            <div className='flex flex-wrap justify-between items-center p-4'>
+                <Link href="/stack" className='text-gray-500'>Stack Used</Link>
+                <Link href="#" className='text-gray-500 mx-2'>|</Link>
+                <Link href="#" className='text-gray-500'>Privacy Policy</Link>
+                <Link href="#" className='text-gray-500 mx-2'>|</Link>
+                <Link href="#" className='text-gray-500'>Terms of Use</Link>
+                <Link href="#" className='text-gray-500 mx-2'>|</Link>
+                <Link href="#" className='text-gray-500'>Contact Us</Link>
+                <Link href="#" className='text-gray-500 mx-2'>|</Link>
+                <Link href="#" className='text-gray-500'>Help</Link>
+                <Link href="#" className='text-gray-500 mx-2'>|</Link>
+                <Link href="#" className='text-gray-500'>About Us</Link>
+                <Link href="#" className='text-gray-500 mx-2'>|</Link>
+                <Link href="#" className='text-gray-500'>Careers</Link>
+                <Link href="#" className='text-gray-500 mx-2'>|</Link>
+                <Link href="#" className='text-gray-500'>Press</Link>
+            </div>
+            <Link href="#" className='text-gray-500'>© 2022 All rights reserved</Link>
+        </div>
     )
 }
 
