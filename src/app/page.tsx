ea7e319e6ba67f3a3d45d91c728a57db72953c6b
ackerman <julio.sergio2709@gmail.com>
2023-06-04 02:16:55 -0400

    ➖: home page layof

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,63 +15,7 @@ export default function HomePage() {
 
   return (
     <div className="flex h-auto flex-col items-center justify-center">
-      <Divider />
-      <ToggleTheme
-        classNameDiv="space-x-4 flex items-center justify-center"
-        classNameLabel="font-semibold"
-      />
-      <Divider />
-      <div className="flex items-center gap-2 flex-wrap justify-center">
-
-        <PostgresIcoSVG />
-        <VercelPGIcoSVG />
-      </div>
-      <Divider />
-      <div className="flex items-center gap-2 flex-wrap justify-center">
-
-        <EsLintIcoSVGs />
-        <EsLintHeaderSVG />
-        <EsLintIcoSVG />
-      </div>
-      <Divider />
-      <div className="flex items-center gap-2 flex-wrap justify-center">
-
-        <RadixIcoSVGs />
-        <RadixHeaderSVG />
-        <RadixIcoSVG />
-      </div>
-      <Divider />
-      <div className="flex items-center gap-2 flex-wrap justify-center">
-
-        <AutoprefixerSVG />
-        <PostCssIco />
-        <TailwindIcoSVG width="200" />
-      </div>
-      <Divider />
-      <div className="flex items-center gap-2 flex-wrap justify-center">
-
-        <NextjsIcoSVG />
-        <NextjsRoundedIcoSVG />
-      </div>
-      <Divider />
-      <div className="flex items-center gap-2 flex-wrap justify-center">
-
-        <TypeScriptIcoSVG />
-        <PrettierIco />
-      </div>
-      <Divider />
-      <div className="flex items-center gap-2 flex-wrap justify-center">
-
-        <TanStackHeader className='h-[58px] flex items-center' />
-        <ZodIcoSVG />
-      </div>
-      <Divider />
-      <div className="flex items-center gap-2 flex-wrap justify-center">
-
-        <ClerkIcoSVG />
-        <UploadThingHeader className='text-5xl h-[58px] flex items-center' />
-      </div>
-      <Divider />
+      Home Page
     </div>
   )
 }
