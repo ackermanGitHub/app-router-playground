f32ee92e68cc176f1069565f15ce9335c8c4432e
ackerman <julio.sergio2709@gmail.com>
2023-06-04 03:02:27 -0400

    ➖: Removing unnecesaries imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-import Divider from "@/components/Divider"
-import ToggleTheme from "@/components/ToggleTheme"
-import {
-  PostgresIcoSVG, VercelPGIcoSVG,
-  EsLintIcoSVGs, EsLintHeaderSVG, EsLintIcoSVG,
-  RadixIcoSVGs, RadixIcoSVG, RadixHeaderSVG,
-  AutoprefixerSVG, PostCssIco, TailwindIcoSVG,
-  NextjsIcoSVG, NextjsRoundedIcoSVG,
-  TypeScriptIcoSVG, PrettierIco,
-  TanStackHeader, ZodIcoSVG,
-  ClerkIcoSVG, UploadThingHeader
-} from "@/utils/SVGs"
+import Footer from '@/components/Footer';
 
 export default function HomePage() {
 
   return (
     <div className="flex h-auto flex-col items-center justify-center">
       Home Page
+      <Footer />
     </div>
   )
 }
