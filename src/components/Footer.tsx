e7a3a7de8ff4f7e13157016e23751785c18d7731
ackerman <julio.sergio2709@gmail.com>
2023-06-06 01:46:29 -0400

    🖌️🔧:todos overflow, adding padding instead of ma

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <footer className='bg-gray-100 dark:bg-gray-900 text-center max-[600px]:mb-20 min-[600px]:ml-20 align-bottom'>
+        <footer className='max-[600px]:mb-20 bg-gray-300 dark:bg-gray-800 text-center'>
             <div className='flex flex-wrap justify-between items-center p-4'>
                 <Link href="/stack" className='text-gray-500'>Stack Used</Link>
                 <Link href="#" className='text-gray-500 mx-2'>|</Link>
@@ -21,7 +21,7 @@ const Footer = () => {
                 <Link href="#" className='text-gray-500 mx-2'>|</Link>
                 <Link href="#" className='text-gray-500'>Press</Link>
             </div>
-            <Link href="#" className='text-gray-500'>© 2022 All rights reserved</Link>
+            <Link href="#" className='text-gray-500'>© 2023 All rights reserved</Link>
         </footer>
     )
 }
