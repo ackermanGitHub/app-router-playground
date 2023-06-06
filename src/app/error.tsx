9bb86f4b2ca2850d54076278438f1928d4590b21
ackerman <julio.sergio2709@gmail.com>
2023-06-06 01:16:03 -0400

    🖌️🔧: loading and error screens height

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -9,7 +9,7 @@ interface errorProps {
 
 const error: React.FC<errorProps> = ({ error, reset }) => {
     return (
-        <div className="flex flex-col items-center justify-center h-[80vh] text-gray-700 dark:text-slate-400">
+        <div className="flex flex-col items-center justify-center max-[600px]:h-[calc(100vh-80px)] min-[600px]:h-screen text-gray-700 dark:text-slate-400">
             <h1 className="text-xl font-bold">{error.name}</h1>
             <h1 className="text-2xl font-bold">Something went wrong</h1>
             <p className="text-lg">{error.message}</p>
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@
 
 const Loading = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-[80vh] text-gray-700 dark:text-slate-400">
+        <div className="max-[600px]:h-[calc(100vh-80px)] min-[600px]:h-screen flex flex-col items-center justify-center text-gray-700 dark:text-slate-400">
             <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-zinc-700 dark:border-zinc-400" />
             <h2 className="text-2xl mt-3">
                 <span className="font-bold">Loading Page</span>
